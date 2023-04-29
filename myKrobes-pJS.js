9c3111c2789f75a0228ba1ba9aa14faa78da9155
mbegam <31596176+mbegam@users.noreply.github.com>
2023-04-29 08:16:31 -0700

    Update myKrobes-pJS.js

--- myKrobes-pJS.js
+++ myKrobes-pJS.js
@@ -100,7 +100,7 @@ var sketchProc = function(processingInstance) {
          // behavior is sensitive to closeness
 
          // dir.mult(-1/2*(closeness*closeness)); 
-         var FC = document.getElementById("force").value;
+         FC = document.getElementById("force").value;
          dir.mult(-FC*(closeness*closeness)); 
          document.getElementById("FC").innerHTML = FC;
 
